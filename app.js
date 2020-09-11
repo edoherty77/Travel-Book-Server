c66eea5b3a4187712e33ef4de599265593751623
Evan Doherty <edoherty77@gmail.com>
2020-09-10 20:07:49 -0400

    app listening on port 4000

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// app
+const express = require('express')
+
+const port = process.env.PORT || 4000;
+const app = express()
+
+
+app.get('/', (req, res)=> {
+  res.send('hello')
+})
+
+app.listen(port, ()=> {
+  console.log(`Api is running on port ${port}`)
+})
