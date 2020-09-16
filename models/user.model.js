bdc1ed4fa96e7ecb07678db1d499741b51f19359
sjaugmented <sjaugmented@gmail.com>
2020-09-16 14:01:28 -0700

    node dependencies

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 const sql = require("./db.js");
 
 // constructor
-const User = function(user) {
+const User = function (user) {
   this.email = user.email;
   this.first_name = user.first_name;
   this.last_name = user.last_name;
@@ -22,8 +22,7 @@ User.create = (newUser, result) => {
   });
 };
 
-
-User.getAll = result => {
+User.getAll = (result) => {
   sql.query("SELECT * FROM users", (err, res) => {
     if (err) {
       console.log("error: ", err);
