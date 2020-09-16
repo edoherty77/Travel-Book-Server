1ed8045c0b5107a56ddcef4420881fb89871d249
sjaugmented <sjaugmented@gmail.com>
2020-09-16 14:23:05 -0700

    merged

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-require('dotenv').config()
-const bodyParser = require('body-parser')
-const routes = require('./routes')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const passport = require('passport')
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+const app = express();
 
-const Trip = require('./models/trip')
+const Trip = require("./models/trip");
 
 // const data = {
 //   name: 'Nicaragua',
@@ -23,33 +23,33 @@ const Trip = require('./models/trip')
 // })
 
 // parse requests of content-type: application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //middleware - session config
 app.use(
   session({
     store: new MongoStore({ url: process.env.MONGODB_URI }),
-    secret: 'duuuude',
+    secret: "duuuude",
     resave: false, // will not resave sessions
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
     },
-  }),
-)
+  })
+);
 
 //middleware - passport config
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
-app.use('/api/v1/trips', routes.trips)
-app.use('/api/v1/users', routes.users)
-app.use('/api/v1/memories', routes.memories)
+app.use("/api/v1/trips", routes.trips);
+// app.use('/api/v1/users', routes.users)
+// app.use('/api/v1/memories', routes.memories)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Api is running on port ${process.env.PORT}`)
-})
+  console.log(`Api is running on port ${process.env.PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  users: require('./users'),
-  trips: require('./trips'),
-  memories: require('./memories')
-}
+  // users: require('./users'),
+  trips: require("./trips"),
+  // memories: require('./memories')
+};
